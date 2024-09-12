0f645ec9dd0dd7c0319f85c914b8ebbd695ad337
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-11 21:30:09 -0700

    home page nav bars

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
     children: React.ReactNode
   }) { return (
         <ClerkProvider>
-            <body className={`${inter.className} bg-dark-1`}>
-                {children}
-            </body>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
         </ClerkProvider>
   )}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import { ClerkProvider, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <>
-      <h1>Threads</h1>
+      <h1 className="head-text text-left">Home</h1>
+      {/* <ClerkProvider afterSignOutUrl="/">
+        <UserButton />
+      </ClerkProvider> */}
     </>
   )
 }
