b2fa7ee0c645fd0e5629c13b1b3f3d0b7938df5a
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-12 14:24:14 -0700

    settings && frontend for account img

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 
 interface Props {
@@ -32,19 +32,37 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle}: Props) => {
+    const [files, setFiles] = useState<File[]>([])
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: "",
-            name: "",
-            username: "",
-            bio: ""
-
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || ""
         }
     });
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if(e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if(!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "";
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file)
+        }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// @ts-check
+ 
+export default (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        images: {
+          remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "img.clerk.com",
+            },
+            {
+              protocol: "https",
+              hostname: "images.clerk.dev",
+            },
+            {
+              protocol: "https",
+              hostname: "uploadthing.com",
+            },
+            {
+              protocol: "https",
+              hostname: "placehold.co",
+            },
+          ],
+        },
+      };
+    return nextConfig
+  }
