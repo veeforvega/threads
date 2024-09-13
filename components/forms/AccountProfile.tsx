90148a9bf5a729bcfe92bfdb4bc9688c9ad670cd
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-12 19:58:36 -0700

    create/update user; create-thread landing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing"
+import { updateUser } from "@/lib/actions/users.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user : {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle}: Props) => {
     const [files, setFiles] = useState<File[]>([])
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -82,7 +86,20 @@ const AccountProfile = ({ user, btnTitle}: Props) => {
             }
         }
 
-        //TODO: Update user profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname,
+        });
+
+        if (pathname == '/profile/edit') {
+            router.back();
+        } else {
+            router.push("/");
+        }
       }
 
     return (
