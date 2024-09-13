6461eed4e3d764da0518818921dc9c7b9a811e0b
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-13 12:57:17 -0700

    fetch posts for home page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,36 @@ export async function createThread({text, author, communityId, path}: Params){
         throw new Error(`Failed to create thread: ${error.message}`)
     }
 };
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    try {
+        connectToDB();
+
+        //Calculate the number of posts to skip
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        //Fetch the posts that have no parents (top-level threads)
+        const postsQuery = Thread.find({ parentId: { $in: [null, undefined] }})
+            .sort({ createdAt: 'desc' })
+            .skip(skipAmount)
+            .limit(pageSize)
+            .populate({ path: 'author', model: User })
+            .populate({ 
+                path: "children", 
+                populate: {
+                    path: "author",
+                    model: User,
+                    select: "_id name parentId image"
+                }
+            });
+        
+        const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] }});
+        const posts = await postsQuery.exec();
+        const isNext = totalPostsCount > skipAmount + posts.length;
+
+        return { posts, isNext }
+
+    } catch (error) {
+        
+    }
+}
