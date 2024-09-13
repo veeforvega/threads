e308aa717841bf9ce6678f225b76bb1d65cb737b
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-13 14:02:40 -0700

    thread card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,39 @@
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs/server";
 import { ClerkProvider, UserButton } from "@clerk/nextjs";
+import ThreadCard from "@/components/cards/ThreadCard";
 
 export default async function Home() {
 
-  // const results = await fetchPosts();
+  const result = await fetchPosts(1, 30);
+  const user =  await currentUser();
+
+  console.log(result);
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      {/* <ClerkProvider afterSignOutUrl="/">
-        <UserButton />
-      </ClerkProvider> */}
+      <section className="mt-9 flex flex-col gap-10">
+        {result?.posts.length == 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result?.posts.map((post) => (
+              <ThreadCard 
+                key={post.id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   )
 }
