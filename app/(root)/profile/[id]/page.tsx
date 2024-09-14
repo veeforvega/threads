7aeed404b01a649939e388f2835d25f533ac6dfd
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-13 20:35:44 -0700

    profile page, tab shadcn view

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,7 +2,10 @@
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import { fetchUser } from "@/lib/actions/users.actions";
 import { currentUser } from "@clerk/nextjs/server";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { redirect } from "next/navigation";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
 
 async function Page({ params }: { params: { id: string }}) {
     const user = await currentUser();
@@ -23,6 +26,31 @@ async function Page({ params }: { params: { id: string }}) {
                 imgUrl={userInfo.image}
                 bio={userInfo.bio}
             />
+
+            <div className='mt-9'>
+                <Tabs defaultValue='threads' className='w-full'>
+                    <TabsList className='tab'>
+                        {profileTabs.map((tab) => (
+                            <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                                <Image
+                                    src={tab.icon}
+                                    alt={tab.label}
+                                    width={24}
+                                    height={24}
+                                    className='object-contain'
+                                />
+                                <p className='max-sm:hidden'>{tab.label}</p>
+
+                                {tab.label === "Threads" && (
+                                    <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                                        {userInfo?.threads.length}
+                                    </p>
+                                )}
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                </Tabs>
+            </div>
         </section>
     )
 
