44cbf6cd53b9ec1e71125a76922394281eff69d1
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-16 19:27:48 -0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,12 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware } from '@clerk/nextjs/server'
 
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+export default authMiddleware({
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
 
-// export default clerkMiddleware((auth, request) => {
-//   if (!isPublicRoute(request)) {
-//     auth().protect()
-//   }
-// })
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// }
-import { clerkMiddleware } from '@clerk/nextjs/server'
-
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
+});
 
 export const config = {
   matcher: [
